0aea913f8fec25dea91c12f94bc80c86c4e2cac2
Alexeykormiltsev <cormiltsev@yandex.ru>
2023-07-27 19:18:37 +0500

    test_2

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,14 @@
-$(function(){
+$(function () {});
 
+window.addEventListener("DOMContentLoaded", function () {
+  const socialMenu = document.querySelector(".social-menu");
+  const toggleButton = document.querySelector(".toggle-menu");
+  const closeButton = document.querySelector(".close");
+
+  toggleButton.addEventListener("click", function () {
+    socialMenu.classList.toggle("show");
+  });
+  closeButton.addEventListener("click", function () {
+    socialMenu.classList.remove("show");
+  });
 });
